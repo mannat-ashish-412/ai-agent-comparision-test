1880baeea22d443721eaa675ce6c7423532b3ac4
Ritin Tiwari <ritintiwari417@gmail.com>
2026-03-02 16:40:33 +0530

    your message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import traceback
 from pydantic_ai import Agent, Tool, UsageLimits
 from dotenv import load_dotenv
+from pydantic_graph import End
 from test_cases.test_runner import run_test
 import asyncio
 import json
@@ -10,7 +11,7 @@ load_dotenv()
 
 async def main(system_prompt, user_prompt, tools, output_type):
     try:
-        MODEL = "gpt-4o-mini"
+        MODEL = "gpt-5-mini"
 
         agent_tools = [Tool(**tool, max_retries=3) for tool in tools]
 
@@ -28,11 +29,18 @@ async def main(system_prompt, user_prompt, tools, output_type):
             usage_limits=UsageLimits(request_limit=50),
         ) as agent_run:
             async for event in agent_run:
-                print("[ EVENT ] \n")
-                print(event)
-                print("-" * 40)
-                pass
-
+                if hasattr(event, "request"):
+                    print("[ Request ] \n")
+                    print(event.request)
+                    print("-" * 40)
+                elif hasattr(event, "response"):
+                    print("[ Response ] \n")
+                    print(event.response)
+                    print("-" * 40)
+                elif isinstance(event, End):
+                    print("[ Final Output ] \n")
+                    print(event.data)
+                    print("-" * 40)
         return agent_run.result.output
     except Exception as e:
         print(traceback.print_exc())
